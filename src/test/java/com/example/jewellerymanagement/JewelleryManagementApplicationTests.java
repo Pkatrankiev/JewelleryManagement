7b013d4b257b4673a2fcdadc2ef74f09201f52df
Катранкиев <PVKatrankiev@koz.npp.bg>
2023-07-10 15:27:39 +0300

    Initial commit

---
+++ src/test/java/com/example/jewellerymanagement/JewelleryManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.jewellerymanagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JewelleryManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
